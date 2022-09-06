82c081fdf0248119df5cce0d8af451579a088bb3
gerald <you@example.com>
2022-09-06 15:08:00 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const arrayOfLetters = word.split('');
+  const reverseLetters = arrayOfLetters.reverse()
+  const reverseString  = reverseLetters.join('')
+
+  if (word == reverseString){
+
+    return true
+
+  } else {
+    return false
+  }
+
 }
+
 
 /* 
   Add your pseudocode here
+  called the function
+  declared the function
+  used a if else statement
 */
 
 /*
   Add written explanation of your solution here
+  i used const to prevent the value from being redefined
+  then went on to use the if else statement 
+
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
+  console.log('Expecting: true')
+  console.log('=>', isPalindrome('abba'));
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log('Expecting: true')
+  console.log('=>', isPalindrome('a'));
 
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log('Expecting: false')
+  console.log('=>', isPalindrome('ab'));
+
+
 }
 
 module.exports = isPalindrome;
